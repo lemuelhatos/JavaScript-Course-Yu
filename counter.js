39fc760aa9b2abd94f0bd5ed9d81772c8cadc3de
lemuelhatos <1hatoslemuel@gmail.com>
2023-05-04 06:46:54 -0700

    JavaScript new commit

--- counter.js
+++ counter.js
@@ -1,18 +1,26 @@
-let counter = 0;
+// let counter = 0;
+if (!localStorage.getItem('counter')) {
+    localStorage.setItem('counter', 0);
+}
 
 function count() {
+    let counter = localStorage.getItem('counter');
     // counter = counter + 1;
     // counter += 1;
     counter++;
     // alert(counter);
     document.querySelector('h1').innerHTML = counter;
+    localStorage.setItem('counter', counter);
 
-    if (counter % 10 === 0) {
-        alert(`Count is now ${counter}`);
-    }
+    // if (counter % 10 === 0) {
+    //     alert(`Count is now ${counter}`);
+    // }
 }
 
 document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('h1').innerHTML = localStorage.getItem('counter');
     document.querySelector('button').onclick = count;
+
+    // setInterval(count, 1000);
 });
 // document.querySelector('button').onclick = count;
